365754229d837c85c3fb44fd77f2e9056b505848
Nuno Susana <nunomsusana@gmail.com>
2021-11-15 10:47:41 +0000

    improved compile script

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 g++ ucantinhos.cpp -o ucantinhos
 echo "Teste 1: "
-./ucantinhos < test1.in
+./ucantinhos < test1.in | grep "3" && echo -e "PASSED\n" || echo -e "FAILED\n"
 echo "Teste 2: "
-./ucantinhos < test2.in
+./ucantinhos < test2.in | grep "13" && echo -e "PASSED\n" || echo -e "FAILED\n"
 echo "Teste 3: "
-./ucantinhos < test3.in
+./ucantinhos < test3.in | grep "257" && echo -e "PASSED\n" || echo -e "FAILED\n"
